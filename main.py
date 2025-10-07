6feae20795774533eb2f6052e62b9d644bcba179
Santhosh796373 <sandysanthosh785@gmail.com>
2025-10-07 22:24:37 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from models import product
+
+app = FastAPI()   # ✅ correct capitalization
+
+@app.get("/")     # 👈 This defines a route
+def greet():
+    return {"message": "hello from WEBSERVER"}
+
+products = [
+    product(1,"Samsung","A Mobile Phone",333.99,4),
+    product(1,"Apple","A Mobile Phone",8833.99,4),
+
+]
+
+
+@app.get("/products")
+def getproducts():
+    return products
