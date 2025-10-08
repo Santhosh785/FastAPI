44f339795ca361afceece61d996817f97043ba2b
Santhosh796373 <sandysanthosh785@gmail.com>
2025-10-08 09:47:58 +0530

    Adding the products and search by id

--- main.py
+++ main.py
@@ -8,12 +8,24 @@ def greet():
     return {"message": "hello from WEBSERVER"}
 
 products = [
-    product(1,"Samsung","A Mobile Phone",333.99,4),
-    product(1,"Apple","A Mobile Phone",8833.99,4),
+    product(id=1,name="Samsung",description="A Mobile Phone",price=333.99,quantity=4),
+    product(id=2,name="Samsung",description="A Mobile Phone",price=333.99,quantity=4),
+    product(id=3,name="Samsung",description="A Mobile Phone",price=333.99,quantity=4),
 
 ]
-
 
 @app.get("/products")
 def getproducts():
     return products
+
+@app.get("/product/{id}")
+def get_product_by_id(id:int):
+    for product in products:
+        if product.id ==id:
+            return product
+        return "Products not Found"
+    
+@app.post("/product")
+def add_products(product:product):
+    products.append(product)
+    return product
